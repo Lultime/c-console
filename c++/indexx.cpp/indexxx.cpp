539ae4c5674961bb7667a3f32edef09c303ea695
Lultime <117547665+Lultime@users.noreply.github.com>
2022-12-08 19:48:05 -0500

    Add files via upload

---
+++ c++/indexx.cpp/indexxx.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main ()
+{
+    int a,b,somme;
+    std::cout<<"Bonjour tout le monde";
+    std::cout<<"Entrer le premier nombre";
+    std::cin>>a;
+    std::cout<<"entrer le deuxieme nombre";
+    std::cin>>b;
+    somme= a + b;
+    std::cout<<somme;
+
+ 
+    return 0;
+
+}
+
---
+++ c++/indexx.cpp/reel.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main ()
+{
+    float a,b,multiple;
+    std::cout<<"Bonjour tout le monde";
+    std::cout<<"Entrer le premier nombre";
+    std::cin>>a;
+    std::cout<<"entrer le deuxieme nombre";
+    std::cin>>b;
+    multiple= a * b;
+    std::cout<<"la multiplication de a * b est"multiple;
+    
+    return 0;
+
+}
+
