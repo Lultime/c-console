2deb312fd11c0008ab43e33972c19302e56123ad
Lultime <117547665+Lultime@users.noreply.github.com>
2023-02-21 21:01:09 -0500

    Add files via upload

---
+++ c++/indexx.cpp/charC++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+int main(void)
+
+{
+	std::string ph;
+char phrase;
+
+std::cout<<"entrez un caractere : ";
+std::cin>>phrase;
+std::cout<<"\n";
+std::cout<<"entrez une phrase : ";
+std::cin>>ph;
+std::cout<<"\n";
+std::cout<<"voici le caractere : ";
+std::cout<<phrase;
+std::cout<<"\n";
+std::cout<<"voici la phrase  : ";
+std::cout<<ph;
+	
+	
+	return 0;
+}
